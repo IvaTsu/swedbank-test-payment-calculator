30fc1202806fac66aabb469ce260f4537ea21fcf
IvaTsu <ivan.tsud@gmail.com>
2018-06-07 02:56:37 +0300

    rough structure of the 1st screen

--- src/static/icons.js
+++ src/static/icons.js
@@ -1,9 +1,20 @@
 const HOME = require('./icon/home.svg');
 const WALLET = require('./icon/wallet.svg');
+// social icons
+const FACEBOOK = require('./icon/facebook.svg');
+const INSTAGRAM = require('./icon/instagram.svg');
+const SKYPE = require('./icon/skype.svg');
+const TWITTER = require('./icon/twitter.svg');
+const YOUTUBE = require('./icon/youtube.svg');
 
 const icons = {
     home: HOME,
     wallet: WALLET,
+    facebook: FACEBOOK,
+    instagram: INSTAGRAM,
+    skype: SKYPE,
+    twitter: TWITTER,
+    youtube: YOUTUBE,
 };
 
 export default icons;
