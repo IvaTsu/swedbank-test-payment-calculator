480105c5c0d80ebef1e005dd6c4cfb8e335ad9de
IvaTsu <ivan.tsud@gmail.com>
2018-06-06 14:46:50 +0300

    add logo

--- src/components/Header/HeaderLogo.js
+++ src/components/Header/HeaderLogo.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
+// assets
 import Images from './../../static/images';
 
 const LOGO = Images.logo;
 
 export default () =>
-    <div>
-        <img src={ LOGO } />
+    <div className="logoContainer">
+        <img
+            src={ LOGO }
+            className="logo"
+        />
     </div>
 ;
