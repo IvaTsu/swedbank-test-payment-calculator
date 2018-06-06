2d7d0c745aaf674ec2a82f1347ab1d53bdb8154f
IvaTsu <ivan.tsud@gmail.com>
2018-06-06 13:46:58 +0300

    add reset.sass

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDom from 'react-dom';
 // Custom Components
 import Header from './components/Header/index';
 
+// styles
+import './reset.sass';
+
 const DOCUMENT_ROOT = document.getElementById("root");
 const App = () => <Header />;
 
