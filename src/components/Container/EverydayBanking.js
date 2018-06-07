d4cf3e27359a4ba15498dbe8618b611384d29ea0
IvaTsu <ivan.tsud@gmail.com>
2018-06-07 14:31:45 +0300

    form skeleton for the Calculator tab section

--- src/components/Container/EverydayBanking.js
+++ src/components/Container/EverydayBanking.js
@@ -45,7 +45,25 @@ class EverydayBanking extends Component {
                 </div>
             </div>
         ) : (
-            <div>Calculator</div>
+            <div>
+                <label htmlFor="loan">Loan size</label>
+                <input name="loan" type="range" />
+                <label htmlFor="period">Period</label>
+                <select name="period">
+                    <option value="30 years">30 years</option>
+                </select>
+                <label htmlFor="interest">Interest</label>
+                <select name="interest">
+                    <option value="4.5%">4.5%</option>
+                </select>
+                <div>
+                    <span>Monthly payment</span>
+                    <span>573.6 EUR</span>
+                    <div>
+                        <button>Apply</button>
+                    </div>
+                </div>
+            </div>
         );
         return (
             <div>
