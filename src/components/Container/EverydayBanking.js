622fb882d2d2210e476adf5d022d4dc45c601fa3
IvaTsu <ivan.tsud@gmail.com>
2018-06-07 12:07:31 +0300

    not full and rough skeleton of Payment tab

--- src/components/Container/EverydayBanking.js
+++ src/components/Container/EverydayBanking.js
@@ -33,6 +33,18 @@ class EverydayBanking extends Component {
                     <button onClick={this._handleCalculatorTabClick}>Calculator</button>
                 </div>
                 {renderTab}
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <h4>Welcome to Swedbank!</h4>
+                        <p>
+                            With 7.2 million private customers and more than 574 000 corporate and organisational customers.
+                            This makes us Sweden's largest bank in terms of number of customers and gives us a leading position in our other home markets of Estonia, Latvia and Lithuania.
+                            As a major bank, we are a significant part of the financial system and play an important role in the local communities we serve.
+                            We are dedicated to helping our customers, our shareholders and society as a whole stay financially sound and sustainable.
+                        </p>
+                    </div>
+                </div>
             </div>
         );
     }
