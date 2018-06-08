fe0a6dde3b1793942119425b48e394204eeb00fc
IvaTsu <ivan.tsud@gmail.com>
2018-06-08 15:31:05 +0300

    start with Calculator styling

--- src/components/Container/Calculator.js
+++ src/components/Container/Calculator.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 export default () => (
-    <div>
-        <label htmlFor="loan">Loan size</label>
-        <input name="loan" type="range" />
-        <label htmlFor="period">Period</label>
-        <select name="period">
-            <option value="30 years">30 years</option>
-        </select>
-        <label htmlFor="interest">Interest</label>
-        <select name="interest">
-            <option value="4.5%">4.5%</option>
-        </select>
+    <div className="calculatorContainer">
+        <div className="loanCalculator">
+            <div>
+                <label htmlFor="loan">Loan size</label>
+                <input name="loan" type="range" />
+            </div>
+            <div>
+                <label htmlFor="period">Period</label>
+                <select name="period">
+                    <option value="30 years">30 years</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="interest">Interest</label>
+                <select name="interest">
+                    <option value="4.5%">4.5%</option>
+                </select>
+            </div>
+        </div>
         <div>
             <span>Monthly payment</span>
             <span>573.6 EUR</span>
